6ab2ef9d714cd2bcdb94bc310aa03b2e9e4b201f
antoine <antoine.imbert@gmail.com>
2014-03-18 04:08:15 +0000

    Add an example that uses rest.AuthBasicMiddleware

---
+++ auth-basic/main.go
@@ -0,0 +1,54 @@
+/* Demonstrate how to setup AuthBasicMiddleware as a pre-routing middleware.
+
+The Curl Demo:
+
+        curl -i http://127.0.0.1:8080/countries
+
+*/
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest"
+	"net/http"
+)
+
+func main() {
+
+	handler := rest.ResourceHandler{
+		PreRoutingMiddlewares: []rest.Middleware{
+			&rest.AuthBasicMiddleware{
+				Realm: "test zone",
+				Authenticator: func(userId string, password string) bool {
+					if userId == "admin" && password == "admin" {
+						return true
+					}
+					return false
+				},
+			},
+		},
+	}
+	handler.SetRoutes(
+		rest.Route{"GET", "/countries", GetAllCountries},
+	)
+	http.ListenAndServe(":8080", &handler)
+}
+
+type Country struct {
+	Code string
+	Name string
+}
+
+func GetAllCountries(w rest.ResponseWriter, r *rest.Request) {
+	w.WriteJson(
+		[]Country{
+			Country{
+				Code: "FR",
+				Name: "France",
+			},
+			Country{
+				Code: "US",
+				Name: "United States",
+			},
+		},
+	)
+}
