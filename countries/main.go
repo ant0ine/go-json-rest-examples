50f81980dd2a607bf9cf9f2e23bd565d922203bc
antoine <antoine.imbert@gmail.com>
2014-04-27 00:12:37 +0000

    Add a sync.RWMutex around the store map.
    Map are not safe for concurrent use without locking.

--- countries/main.go
+++ countries/main.go
@@ -18,13 +18,14 @@ package main
 import (
 	"github.com/ant0ine/go-json-rest/rest"
 	"net/http"
+	"sync"
 )
 
 func main() {
 
 	handler := rest.ResourceHandler{
-                EnableRelaxedContentType: true,
-        }
+		EnableRelaxedContentType: true,
+	}
 	handler.SetRoutes(
 		&rest.Route{"GET", "/countries", GetAllCountries},
 		&rest.Route{"POST", "/countries", PostCountry},
@@ -41,9 +42,13 @@ type Country struct {
 
 var store = map[string]*Country{}
 
+var lock = sync.RWMutex{}
+
 func GetCountry(w rest.ResponseWriter, r *rest.Request) {
 	code := r.PathParam("code")
+	lock.RLock()
 	country := store[code]
+	lock.RUnlock()
 	if country == nil {
 		rest.NotFound(w, r)
 		return
@@ -52,12 +57,14 @@ func GetCountry(w rest.ResponseWriter, r *rest.Request) {
 }
 
 func GetAllCountries(w rest.ResponseWriter, r *rest.Request) {
+	lock.RLock()
 	countries := make([]*Country, len(store))
 	i := 0
 	for _, country := range store {
 		countries[i] = country
 		i++
 	}
+	lock.RUnlock()
 	w.WriteJson(&countries)
 }
 
@@ -76,11 +83,15 @@ func PostCountry(w rest.ResponseWriter, r *rest.Request) {
 		rest.Error(w, "country name required", 400)
 		return
 	}
+	lock.Lock()
 	store[country.Code] = &country
+	lock.Unlock()
 	w.WriteJson(&country)
 }
 
 func DeleteCountry(w rest.ResponseWriter, r *rest.Request) {
 	code := r.PathParam("code")
+	lock.Lock()
 	delete(store, code)
+	lock.Unlock()
 }
