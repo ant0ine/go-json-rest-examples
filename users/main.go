fa9fdec795e5d32fdfacdb7173fcf677525d42f3
antoine <antoine.imbert@gmail.com>
2014-05-10 22:33:55 +0000

    Convert the users example to the new documentation format

--- users/main.go
+++ users/main.go
@@ -1,17 +1,3 @@
-/* Demonstrate how to use rest.RouteObjectMethod
-
-rest.RouteObjectMethod helps create a Route that points to
-an object method instead of just a function.
-
-The curl demo:
-
-        curl -i -d '{"Name":"Antoine"}' http://127.0.0.1:8080/users
-        curl -i http://127.0.0.1:8080/users/0
-        curl -i -X PUT -d '{"Name":"Antoine Imbert"}' http://127.0.0.1:8080/users/0
-        curl -i -X DELETE http://127.0.0.1:8080/users/0
-        curl -i http://127.0.0.1:8080/users
-
-*/
 package main
 
 import (
