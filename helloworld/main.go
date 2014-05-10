cd94f31602bb6a3167aa9f89073bc35bc95c0f98
antoine <antoine.imbert@gmail.com>
2014-05-10 22:23:09 +0000

    Replace the docstring by the README.md
    A new tool will assemble all that in the go-json-rest/README.md

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,14 +1,8 @@
-/* Tradition!
-
-The curl demo:
-
-        curl -i http://127.0.0.1:8080/message
-
-*/
 package main
 
 import (
 	"github.com/ant0ine/go-json-rest/rest"
+	"log"
 	"net/http"
 )
 
@@ -18,12 +12,15 @@ type Message struct {
 
 func main() {
 	handler := rest.ResourceHandler{}
-	handler.SetRoutes(
+	err := handler.SetRoutes(
 		&rest.Route{"GET", "/message", func(w rest.ResponseWriter, req *rest.Request) {
 			w.WriteJson(&Message{
 				Body: "Hello World!",
 			})
 		}},
 	)
-	http.ListenAndServe(":8080", &handler)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(http.ListenAndServe(":8080", &handler))
 }
